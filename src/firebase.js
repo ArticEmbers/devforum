31fb6c039bac5f8f24611294ec6508c263211d0e
Aeon <imabandoned591@gmail.com>
2025-10-20 18:55:24 +0200

    Add persistent login with Luna and Amy users

--- src/firebase.js
+++ src/firebase.js
@@ -2,18 +2,21 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc, deleteDoc, doc, onSnapshot, query, orderBy, serverTimestamp } from "firebase/firestore";
 
+// Firebase config
 const firebaseConfig = {
     apiKey: "AIzaSyAUJtKUZtF9hjC3-QZv0_Bf6DltMNVJDhc",
     authDomain: "devforum-2e7e9.firebaseapp.com",
     projectId: "devforum-2e7e9",
-    storageBucket: "devforum-2e7e9.firebasestorage.app",
+    storageBucket: "devforum-2e7e9.appspot.com",
     messagingSenderId: "611187822619",
     appId: "1:611187822619:web:1543599b010ad2b970a8be",
     measurementId: "G-FJ7Z517V4R"
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Firestore database
 const db = getFirestore(app);
 
 export { db, collection, addDoc, deleteDoc, doc, onSnapshot, query, orderBy, serverTimestamp };
